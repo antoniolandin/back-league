17c0b3f6e587c11265490e3d7fb3b461044f4211
antonio <antoniolandin@proton.me>
2024-11-19 15:24:22 +0100

    fix(scripts): port

--- scripts/fill.sh
+++ scripts/fill.sh
@@ -2,8 +2,8 @@
 # Description: Script para rellenar la base de datos con los jugadores y equipos por defecto
 # Se necesita el servidor de la api levantado
 
- curl -X POST "http://localhost:3000/api/equipos" -H "Content-Type: application/json" -d '{"nombre":"Wolfram City"}'
+ curl -X POST "http://localhost:3500/api/equipos" -H "Content-Type: application/json" -d '{"nombre":"Wolfram City"}'
 
- curl -X POST "http://localhost:3000/api/jugadores" -H "Content-Type: application/json" -d '{"id_equipo":1, "nombre":"Antonio", "primer_apellido":"Cabrera"}'
- curl -X POST "http://localhost:3000/api/jugadores" -H "Content-Type: application/json" -d '{"id_equipo":1, "nombre":"Antonio", "primer_apellido":"Pérez"}'
- curl -X POST "http://localhost:3000/api/jugadores" -H "Content-Type: application/json" -d '{"id_equipo":1, "nombre":"Alvaro", "primer_apellido":"Salís"}'
+ curl -X POST "http://localhost:3500/api/jugadores" -H "Content-Type: application/json" -d '{"id_equipo":1, "nombre":"Antonio", "primer_apellido":"Cabrera"}'
+ curl -X POST "http://localhost:3500/api/jugadores" -H "Content-Type: application/json" -d '{"id_equipo":1, "nombre":"Antonio", "primer_apellido":"Pérez"}'
+ curl -X POST "http://localhost:3500/api/jugadores" -H "Content-Type: application/json" -d '{"id_equipo":1, "nombre":"Alvaro", "primer_apellido":"Salís"}'
