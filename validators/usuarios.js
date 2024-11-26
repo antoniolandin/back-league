5b307629d8706c9a00ca4587d44f45bb50c671c0
antonio <antoniolandin@proton.me>
2024-11-26 15:48:01 +0100

    fix(validators): usuarios

--- validators/usuarios.js
+++ validators/usuarios.js
@@ -2,16 +2,19 @@ const { check } = require('express-validator');
 const validateResults = require('../utils/handleValidator');
 
 const validatorRegister = [
-    check('nombre').exists().withMessage("El nombre es obligatorio").
-        notEmpty().withMessage("El nombre no puede estar vacío").
+    check('nombre').trim().escape().
+        exists().withMessage("El nombre es obligatorio").bail().
+        notEmpty().withMessage("El nombre no puede estar vacío").bail().
         isLength({ max: 15, min: 3 }).withMessage("El nombre debe tener entre 3 y 15 carácteres"),
 
-    check('email').exists().withMessage("El email es obligatorio").
-        notEmpty().withMessage("El email no puede estar vacío").
+    check('email').trim().escape().
+        exists().trim().withMessage("El email es obligatorio").bail().
+        notEmpty().withMessage("El email no puede estar vacío").bail().
         isEmail().withMessage("El email es incorrecto"),
 
-    check('contraseña').exists().withMessage("La contraseña es obligatoria").
-        notEmpty().withMessage("La contraseña no puede estar vacía").
+    check('contraseña').trim().escape().
+        exists().withMessage("La contraseña es obligatoria").bail().
+        notEmpty().withMessage("La contraseña no puede estar vacía").bail().
         isLength({ max: 50, min: 3 }).withMessage("La contraseña tiene que tener entre 3 y 50 carácteres"),
 
     (req, res, next) => {
@@ -20,12 +23,15 @@ const validatorRegister = [
 ]
 
 const validatorLogin = [
-    check('email').exists().withMessage("El email es obligatorio").
-        notEmpty().withMessage("El email no puede estar vacío").
+    check('email').trim().escape().
+        exists().trim().withMessage("El email es obligatorio").bail().
+        notEmpty().withMessage("El email no puede estar vacío").bail().
         isEmail().withMessage("El email es incorrecto"),
 
-    check('contraseña').exists().withMessage("La contraseña es obligatoria").
-        notEmpty().withMessage("La contraseña no puede estar vacía"),
+    check('contraseña').trim().escape().
+        exists().withMessage("La contraseña es obligatoria").bail().
+        notEmpty().withMessage("La contraseña no puede estar vacía").bail().
+        isLength({ max: 50, min: 3 }).withMessage("La contraseña tiene que tener entre 3 y 50 carácteres"),
 
     (req, res, next) => {
         return validateResults(req, res, next)
