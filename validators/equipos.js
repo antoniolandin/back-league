036da76b62cebfa03aa4b0f585d9bd74469fd1f6
antonio <antoniolandin@proton.me>
2024-11-26 20:43:24 +0100

    feat(validators): mejorado equipos y jugadores

--- validators/equipos.js
+++ validators/equipos.js
@@ -2,16 +2,39 @@ const { check } = require('express-validator');
 const validateResults = require('../utils/handleValidator');
 
 const validatorCreateEquipo = [
-    check('nombre').exists().notEmpty(),
-    check('nombre').isLength({ max: 15 }),
-    check('nombre').isLength({ min: 3 }),
-    check('partidos_jugados'),
-    check('victorias'),
-    check('derrotas'),
-    check('empates'),
-    check('puntos'),
-    check('goles_favor'),
-    check('goles_contra'),
+    check('nombre').trim().escape().
+        exists().withMessage("El nombre es obligatorio").bail().
+        notEmpty().withMessage("El nombre no puede estar vacío").bail().
+        isLength({ min: 2, max: 15 }).withMessage("El nombre debe tener entre 2 y 15 carácteres"),
+
+    check('partidos_jugados').trim().escape().optional().
+        notEmpty().withMessage("Los partidos jugados no pueden estar vacíos").bail().
+        isInt({ min: 0 }).withMessage("Los partidos jugados deben ser un entero positivo"),
+
+    check('victorias').trim().escape().optional().
+        notEmpty().withMessage("Las victorias no pueden estar vacías").bail().
+        isInt({ min: 0 }).withMessage("Las victorias deben ser un entero positivo"),
+
+    check('derrotas').trim().escape().optional().
+        notEmpty().withMessage("Las derrotas no pueden estar vacías").bail().
+        isInt({ min: 0 }).withMessage("Las derrotas deben ser un entero positivo"),
+
+    check('empates').trim().escape().optional().
+        notEmpty().withMessage("Los empates no pueden estar vacíos").bail().
+        isInt({ min: 0 }).withMessage("Los empates deben ser un entero positivo"),
+
+    check('puntos').trim().escape().optional().
+        notEmpty().withMessage("Los puntos no pueden estar vacíos").bail().
+        isInt({ min: 0 }).withMessage("Los puntos deben ser un entero positivo"),
+
+    check('goles_favor').trim().escape().optional().
+        notEmpty().withMessage("Los goles a favor no pueden estar vacíos").bail().
+        isInt({ min: 0 }).withMessage("Los goles a favor deben ser un entero positivo"),
+
+    check('goles_contra').trim().escape().optional().
+        notEmpty().withMessage("Los goles en contra no pueden estar vacíos").bail().
+        isInt({ min: 0 }).withMessage("Los goles en contra deben ser un entero positivo"),
+
     (req, res, next) => {
         return validateResults(req, res, next)
     }
--- validators/jugadores.js
+++ validators/jugadores.js
@@ -2,17 +2,42 @@ const { check } = require('express-validator');
 const validateResults = require('../utils/handleValidator');
 
 const validatorCreateJugador = [
-    check('id_equipo').exists().notEmpty().isInt(),
-    check('nombre').exists().notEmpty(),
-    check('primer_apellido').exists().notEmpty(),
-    check('segundo_apellido'),
-    check('grado'),
-    check('curso'),
-    check('goles'),
-    check('partidos_jugados'),
-    (req, res, next) => {
-        return validateResults(req, res, next)
-    }
+    check('id_equipo').trim().escape().
+        exists().withMessage("El id del equipo es obligatorio").bail().
+        notEmpty().withMessage("El id del equipo no puede estar vacío").bail().
+        isInt({ min: 1 }).withMessage("El id del equipo debe ser un entero mayor que cero"),
+
+    check('nombre').trim().escape().
+        exists().withMessage("El nombre es obligatorio").bail().
+        notEmpty().withMessage("El nombre no puede estar vacío"),
+
+    check('primer_apellido').trim().escape().optional().
+        notEmpty().withMessage("El primer apellido no puede estar vacío").bail().
+        isLength({ min: 2, max: 15 }).withMessage("El primer apellido debe tener entre 2 y 15 carácteres"),
+
+    check('segundo_apellido').trim().escape().optional().
+        notEmpty().withMessage("El segundo apellido no puede estar vacío").bail().
+        isLength({ min: 2, max: 15 }).withMessage("El segundo apellido debe tener entre 2 y 15 carácteres"),
+
+    check('grado').trim().escape().optional().
+        notEmpty().withMessage("El grado no puede estar vacío").bail().
+        isLength({ min: 2, max: 15 }).withMessage("El grado debe tener entre 2 y 15 carácteres"),
+
+    check('curso').trim().escape().optional().
+        notEmpty().withMessage("El curso no puede estar vacío").bail().
+        isInt({ min: 0 }).withMessage("El curso debe ser un entero positivo"),
+
+    check('goles').trim().escape().optional().
+        notEmpty().withMessage("Los goles no pueden estar vacíos").bail().
+        isInt({ min: 0 }).withMessage("Los goles deben ser un entero positivo"),
+
+    check('partidos_jugados').trim().escape().optional().
+        notEmpty().withMessage("Los partidos jugados no pueden estar vacíos").bail().
+        isInt({ min: 0 }).withMessage("Los partidos jugados deben ser un entero positivo"),
+
+        (req, res, next) => {
+    return validateResults(req, res, next)
+}
 ]
 
 module.exports = { validatorCreateJugador }
