4db3f6eb54798a13adb2f8d37c903e2dd1054753
aperezmarquez <aperezmarquez.business@gmail.com>
2024-11-29 18:20:41 +0100

    Modificado el post del equipos_fantasy y su validator

--- controllers/fantasy_equipos/postEquipo.js
+++ controllers/fantasy_equipos/postEquipo.js
@@ -1,6 +1,5 @@
 const { matchedData } = require("express-validator")
-const { fantasy_equipos } = require("../../models")
-const { Usuarios } = require("../../models")
+const { FantasyEquipos } = require("../../models")
 const handleError = require("../../utils/handleError")
 
 const postEquipo = async (req, res) => {
@@ -12,17 +11,7 @@ const postEquipo = async (req, res) => {
             return
         }
 
-        const user = await Usuarios.findByPk(body.id_user).then(function (user) {
-            if (!user) {
-                handleError(res, "El usuario no existe", 404)
-                return
-            }
-        }).catch(function (err) {
-            handleError(res, err, 400)
-            return
-        })
-
-        const result = await equipos_fantasy.create(body).then(function (result) {
+        const result = await FantasyEquipos.create(body).then(function (result) {
             if (result) {
                 res.status(200).json(result)
             } else {
--- validators/fantasy_equipos.js
+++ validators/fantasy_equipos.js
@@ -4,10 +4,6 @@ const validateResults = require("../utils/handleValidator")
 const handleError = require("../utils/handleError")
 
 const validatorCreateEquipoFantasy = [
-    check('id_user').trim().escape()
-        .exists().withMessage("El id_user es obligatorio").bail()
-        .notEmpty().withMessage("El id_user no puede estar vací")
-        .isInt().withMessage("El id_user debe ser un número"),
     check('nombre').trim().escape()
         .exists().withMessage("El nombre es obligatorio").bail()
         .notEmpty().withMessage("El nombre no puede estar vacío"),
