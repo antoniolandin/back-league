c9e6d5cd891a4bc0221882e0f56487b2719d4804
aperezmarquez <aperezmarquez.business@gmail.com>
2024-12-17 20:46:45 +0100

    Fixed getPartidos
    Ya devuelve correctamente los datos en development

--- controllers/partido/getPartidos.js
+++ controllers/partido/getPartidos.js
@@ -1,51 +1,54 @@
 const { Partidos } = require('../../models')
 const handleError = require('../../utils/handleError')
+const { Op } = require('sequelize')
 
 const getPartidos = async (req, res) => {
     try {
-        const partidos = await Partidos.findAll()
+        const fechaActual = new Date()
+
+        const partidos = await Partidos.findAll({
+            where: {
+                fecha: {
+                    [Op.lt]: fechaActual
+                }
+            }
+        })
         
         const json = []
-        var a = 0
 
-        // Iterar sobre los partidos usando for...of
         for (const partido of partidos) {
-            // Accede a los equipos relacionados
             const equipos = await partido.getEquipos({
                 through: { attributes: ['goles'] }
             })
+            
+            const partido_data = partido.dataValues
+            const equipo_uno = equipos[0].dataValues
+            const equipo_dos = equipos[1].dataValues
 
-            // Verificar si el partido ya ocurrió
-            if (new Date(partido.fecha) < new Date()) {
-                // Extraer los goles desde la tabla intermedia EquiposPartidos
-                const goles_1 = equipos[0]?.EquiposPartidos?.goles || 0; // Validar que existan valores
-                const goles_2 = equipos[1]?.EquiposPartidos?.goles || 0;
+            const goles_uno = equipos[0]?.EquiposPartidos?.goles || 0
+            const goles_dos = equipos[1]?.EquiposPartidos?.goles || 0
 
-                // Determinar el equipo ganador
-                let equipo_ganador = null;
-                if (goles_1 > goles_2) {
-                    equipo_ganador = equipos[0].dataValues;
-                } else if (goles_2 > goles_1) {
-                    equipo_ganador = equipos[1].dataValues;
-                }
-                
-                partido_data = partido.dataValues
-                equipo_1 = equipos[0].dataValues
-                equipo_2 = equipos[1].dataValues
-
-                // Construir el objeto JSON
-                json.push({
-                    id: partido_data.id,
-                    equipo_uno: equipo_1.nombre,
-                    equipo_dos: equipo_2.nombre,
-                    equipo_ganador: equipo_ganador.nombre,
-                    goles_uno: goles_1,
-                    goles_dos: goles_2,
-                    fecha: partido_data.fecha 
-                });
-
-                a = a + 1
+            console.log(goles_uno, goles_dos)
+            // Determinar el equipo ganador
+            let equipo_ganador = null;
+            if (goles_uno > goles_dos) {
+                equipo_ganador = equipo_uno;
+            } else if (goles_dos > goles_uno) {
+                equipo_ganador = equipo_dos;
+            } else {
+                equipo_ganador = { nombre: 'Empate' };
             }
+
+            // Construir el objeto JSON
+            json.push({
+                id: partido_data.id,
+                equipo_uno: equipo_uno.nombre,
+                equipo_dos: equipo_dos.nombre,
+                equipo_ganador: equipo_ganador.nombre,
+                goles_uno: goles_uno,
+                goles_dos: goles_dos,
+                fecha: partido_data.fecha 
+            })
         }
 
         res.status(200).json(json);
