35b587d43740df35c78501c13d12996f06a3087b
antonio <antoniolandin@proton.me>
2024-12-18 00:03:26 +0100

    fix(controllers): getEquipos return jugadores

--- controllers/fantasy_equipos/getEquipos.js
+++ controllers/fantasy_equipos/getEquipos.js
@@ -1,17 +1,42 @@
-const { FantasyEquipos } = require("../../models")
+const { FantasyEquipos, Jugadores } = require("../../models")
 const handleError = require("../../utils/handleError")
 
 const getEquipos = async (req, res) => {
     try {
-        const result = await FantasyEquipos.findAll({
+        const equipos = await FantasyEquipos.findAll({
             order: [['puntos', 'DESC']]
-        }).then(function (result) {
-            if (result.length > 0) {
-                res.status(200).json(result)
-            } else {
-                handleError(res, "No hay equipos", 404)
-            }
         })
+
+        // si el equipo tiene jugadores
+        if (equipos.length > 0) {
+            // declaramos el array de los json que devolveremos 
+            const equipos_json = []
+
+            // iteramos sobre los equipos
+            for (i = 0; i < equipos.length; i++) {
+                // obtenemos los jugadores de la BBDD
+                const jugadores = await equipos[i].getJugadores()
+
+                // filtramos los jugadores para quedarnos solo con los campos importantes
+                const jugadores_filtrados = jugadores.map(jugador => ({
+                    id: jugador.id,
+                    nombre: jugador.nombre
+                }))
+
+                // pasamos el equipo a JSON y añadimos un campo nuevo con los jugadores
+                const equipo = equipos[i].toJSON()
+                equipo.jugadores = jugadores_filtrados
+
+                // añadimos el equipo al array
+                equipos_json.push(equipo)
+            }
+
+            // enviamos la respuesta
+            res.status(200).json(equipos_json)
+        } else {
+            handleError(res, "No hay equipos", 404)
+        }
+
     } catch (error) {
         handleError(res, error, 400)
     }
